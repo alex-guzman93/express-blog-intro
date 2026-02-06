a9f47e1d11b3b15100ae8ceeeeddc1753928eaec
alexander guzman <Alex.networking93@gmail.com>
2026-02-06 17:33:50 +0100

    Add folder public + img and array oggetto post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
- // rotta index app
+// rotta index app
 app.get('/', (req, res) => {
     res.send("<h1>Server del mio blog</h1>")
 })
@@ -11,6 +11,42 @@ app.get('/', (req, res) => {
 // rotta port
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
-    
+
 })
 
+// Array oggetto lista dei post
+
+const post = [
+    {
+        id: 1,
+        title: "",
+        content:
+            immage: "",
+        tags: ["", ""],
+    }, {
+        id: 2,
+        title: "",
+        content:
+            immage: "",
+        tags: ["", "", "no"],
+    }, {
+        id: 3,
+        title: "",
+        content:
+            immage: "",
+        tags: ["o", "", ""],
+    }, {
+        id: 4,
+        title: "",
+        content:
+            immage: "",
+        tags: ["", ""],
+    }, {
+        id: 5,
+        title: "",
+        content:
+            immage: "",
+        tags: ["", "", "", "", ""],
+    }
+];
+res.json(post)
